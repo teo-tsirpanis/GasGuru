29730443ec341c9ceff725f93c68297951d98135
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-04-16 01:43:47 +0300

    Fix some file names.

---
+++ src/GasGuru.Api/GasStationOptionsModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GasGuru.Api;
+
+public class GasStationOptionsModel
+{
+    [Range(0, 100_000_000.0)]
+    public decimal MonthlyRent { get; set; }
+}
--- src/GasGuru.Api/ModelSerializerContext.cs
+++ src/GasGuru.Api/ModelSerializerContext.cs
@@ -1,24 +1,15 @@
-﻿#nullable disable
-
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace GasGuru.Api;
 
 [JsonSerializable(typeof(CustomerViewModel))]
 [JsonSerializable(typeof(CustomerEditModel))]
-[JsonSerializable(typeof(GasStationOptionsModel))]
 [JsonSerializable(typeof(EmployeeViewModel))]
 [JsonSerializable(typeof(EmployeeEditModel))]
 [JsonSerializable(typeof(ItemViewModel))]
 [JsonSerializable(typeof(ItemEditModel))]
 [JsonSerializable(typeof(TransactionViewModel))]
 [JsonSerializable(typeof(TransactionCreateModel))]
+[JsonSerializable(typeof(GasStationOptionsModel))]
 [JsonSourceGenerationOptions(DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
 public partial class ModelSerializerContext : JsonSerializerContext { }
-
-public class GasStationOptionsModel
-{
-    [Range(0, 100_000_000.0)]
-    public decimal MonthlyRent { get; set; }
-}
