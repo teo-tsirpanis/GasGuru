dd025c7f8ff836ac0b4000426bf473a3e9763a79
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-04-15 15:30:38 +0300

    Add a gas station options controller.

---
+++ src/GasGuru.Web.Api/Controllers/GasStationOptionsController.cs
@@ -0,0 +1,28 @@
+﻿using GasGuru.Api;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GasGuru.Web.Api.Controllers;
+
+[Route("api/options")]
+[ApiController]
+public class GasStationOptionsController : ControllerBase
+{
+    private readonly IGasStationOptionsRepo _repo;
+
+    public GasStationOptionsController(IGasStationOptionsRepo repo)
+    {
+        _repo = repo;
+    }
+
+    [HttpGet]
+    public Task<GasStationOptionsModel> Get()
+    {
+        return _repo.GetOptionsAsync();
+    }
+
+    [HttpPut]
+    public async Task Put([FromBody] GasStationOptionsModel model)
+    {
+        await _repo.UpdateOptionsAsync(model);
+    }
+}
