8ce3ce6670ebc5b2bcdcde8146e2ca98e757ce15
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-04-18 01:38:18 +0300

    Add a comment.

--- src/GasGuru.Api/IEntityRepo.cs
+++ src/GasGuru.Api/IEntityRepo.cs
@@ -2,6 +2,8 @@
 
 public interface IEntityRepo<TViewModel, TEditModel>
 {
+    // I could have made it return a sequence of GUIDs to avoid sending
+    // too much data, but it would have been susceptible to N+1 queries.
     IAsyncEnumerable<TViewModel> GetAllAsync(bool includeDeleted);
     Task<TViewModel?> GetByIdAsync(Guid id);
     Task CreateAsync(TEditModel entity);
